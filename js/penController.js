286ba5cbaa5210cf722d58ff8a0f56ec46cc9b9e
JohnnyBaptista <joaosbaptista@hotmail.com>
2022-04-01 19:15:45 -0300

    pen controller

---
+++ js/penController.js
@@ -0,0 +1,26 @@
+const fabric  = window.fabric;
+
+export const changePenColor = (color, canvas) => {
+    console.log({aq: canvas})
+    switch (color) {
+        case 'r':
+            canvas.freeDrawingBrush.color = '#e84d4d';
+            canvas.freeDrawingCursor = 'url(./assets/red.png) 10 50, auto';
+            break;
+        case 'g':
+            canvas.freeDrawingBrush.color = '#7df279';
+            canvas.freeDrawingCursor = 'url(./assets/lime.png) 10 50, auto';
+            break;
+        case 'b':
+            canvas.freeDrawingBrush.color = '#6c73f5';
+            canvas.freeDrawingCursor = 'url(./assets/blue.png) 10 50, auto';
+            break;
+        case 'd':
+            canvas.freeDrawingBrush.color = 'black';
+            canvas.freeDrawingCursor = 'url(./assets/pink.png) 10 50, auto';
+            break;
+        default:
+            break;
+        }
+    return canvas;
+}
