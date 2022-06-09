218d94b481e58b8449ea9ff38a4493ecf64caa27
jbaptista2 <jbaptista@gobots.com.br>
2022-06-09 20:14:44 -0300

    adicionando redo e undo

--- js/canvasController.js
+++ js/canvasController.js
@@ -1,25 +1,41 @@
-export const changePenColor = (color, canvas) => {
-    switch (color) {
-        case 'r':
-            canvas.freeDrawingBrush.color = '#e84d4d';
-            canvas.freeDrawingCursor = 'url(./assets/red.png) 10 50, auto';
-            break;
-        case 'g':
-            canvas.freeDrawingBrush.color = '#7df279';
-            canvas.freeDrawingCursor = 'url(./assets/lime.png) 10 50, auto';
-            break;
-        case 'b':
-            canvas.freeDrawingBrush.color = '#6c73f5';
-            canvas.freeDrawingCursor = 'url(./assets/blue.png) 10 50, auto';
-            break;
-        case 'd':
-            canvas.freeDrawingBrush.color = 'black';
-            canvas.freeDrawingCursor = 'url(./assets/pink.png) 10 50, auto';
-            break;
-        default:
-            break;
-        }
-    return canvas;
-}
+const changePenColor = (color, canvas) => {
+  switch (color) {
+    case "r":
+      canvas.freeDrawingBrush.color = "#e84d4d";
+      canvas.freeDrawingCursor = "url(./assets/red.png) 10 50, auto";
+      break;
+    case "g":
+      canvas.freeDrawingBrush.color = "#7df279";
+      canvas.freeDrawingCursor = "url(./assets/lime.png) 10 50, auto";
+      break;
+    case "b":
+      canvas.freeDrawingBrush.color = "#6c73f5";
+      canvas.freeDrawingCursor = "url(./assets/blue.png) 10 50, auto";
+      break;
+    case "d":
+      canvas.freeDrawingBrush.color = "black";
+      canvas.freeDrawingCursor = "url(./assets/pink.png) 10 50, auto";
+      break;
+    default:
+      break;
+  }
+  return canvas;
+};
 
-export const clearCanvas = (canvas) => canvas.clear();
+const clearCanvas = (canvas) => canvas.clear();
+
+const undo = (canvas, canvasObjectsList) => {
+  if (canvas._objects.length > 0) {
+    canvasObjectsList.push(canvas._objects.pop());
+    canvas.renderAll();
+  }
+};
+
+const redo = (canvas, canvasObjectsList, isRedoing) => {
+  if (canvasObjectsList.length > 0) {
+    canvas.add(canvasObjectsList.pop());
+  }
+};
+
+
+export { changePenColor, clearCanvas, undo, redo };
