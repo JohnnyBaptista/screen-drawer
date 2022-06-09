3117de2e41966240d1093ddb335f2d9ea9500db3
jbaptista2 <jbaptista@gobots.com.br>
2022-06-09 20:14:59 -0300

    chamando undo e redo

--- js/app.js
+++ js/app.js
@@ -1,43 +1,51 @@
-import { changePenColor, clearCanvas } from './canvasController.js';
-const fabric  = window.fabric;
+import { changePenColor, clearCanvas, redo, undo } from "./canvasController.js";
+const fabric = window.fabric;
 let canvas = null;
+const canvasObjectsList = [];
+const isRedoing = false;
 
 const initCanvas = () => {
-    const fabricCanvas = new fabric.Canvas("mainCanvas", {
-        width: window.innerWidth,
-        height: window.innerHeight,
-        backgroundColor: "transparent",
-        isDrawingMode: true,
-        freeDrawingCursor: 'url(./assets/pink.png) 10 50, auto',
-        freeDrawingBrush: new fabric.PencilBrush(this),
-      });
+  const fabricCanvas = new fabric.Canvas("mainCanvas", {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: "transparent",
+    isDrawingMode: true,
+    freeDrawingCursor: "url(./assets/pink.png) 10 50, auto",
+    freeDrawingBrush: new fabric.PencilBrush(this),
+  });
 
-    canvas = fabricCanvas.getSelectionElement().parentNode;
-    fabricCanvas.freeDrawingBrush.width = 5;
-    if(canvas.requestFullScreen) {
-        canvas.requestFullScreen();
-    }
-    else if(canvas.webkitRequestFullScreen) {
-        canvas.webkitRequestFullScreen();
-    }
-    else if(canvas.mozRequestFullScreen) {
-        canvas.mozRequestFullScreen();
-    }
-    return { canvas, fabricCanvas }
-}
-
+  canvas = fabricCanvas.getSelectionElement().parentNode;
+  fabricCanvas.freeDrawingBrush.width = 5;
+  if (canvas.requestFullScreen) {
+    canvas.requestFullScreen();
+  } else if (canvas.webkitRequestFullScreen) {
+    canvas.webkitRequestFullScreen();
+  } else if (canvas.mozRequestFullScreen) {
+    canvas.mozRequestFullScreen();
+  }
+  return { canvas, fabricCanvas };
+};
+// can = fabricCanvas
 export const addListeners = (can) => {
-    window.addEventListener('keydown', (e) => {
-        const keyName = e.key;
-        if(keyName === 'Escape' || keyName === 'c') {
-            clearCanvas(can);
-        } else {
-            can = changePenColor(keyName, can);
-        }
-    });
-}
+  window.addEventListener("keydown", (e) => {
+    const keyName = e.key;
+    if (keyName === "Escape" || keyName === "c") {
+      clearCanvas(can);
+    } else {
+      can = changePenColor(keyName, can);
+    }
+    if (e.ctrlKey) {
+      if (e.key === "z") {
+        undo(can, canvasObjectsList);
+      }
+      if (e.key === "y") {
+        redo(can, canvasObjectsList);
+      }
+    }
+  });
+};
 
 export const init = () => {
-    const { fabricCanvas } = initCanvas();
-    addListeners(fabricCanvas);
-}
+  const { fabricCanvas } = initCanvas();
+  addListeners(fabricCanvas);
+};
